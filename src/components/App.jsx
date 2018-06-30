eb8adca18a65e497894f894a791c517cf495c45e
Aayush Kapoor <xeoneux@icloud.com>
2018-06-30 13:13:54 +0530

    Inject custom styles for main coin list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,43 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <h1>Welcome to React</h1>;
+    return (
+      <main>
+        <section class="jumbotron">
+          <h1 class="jumbotron-heading">
+            Generation Blockchain Crypto Tracker
+          </h1>
+
+          <p class="lead text-muted">Track your favourite cryptocurrencies</p>
+
+          <div class="row">
+            <ul>
+              <li class="coin-item">
+                <div class="coin-image">
+                  <img
+                    alt="Placeholder"
+                    src="http://placeholder.pics/svg/300"
+                  />
+                </div>
+
+                <div class="coin-name">
+                  <p>BITCOIN</p>
+                </div>
+
+                <div class="coin-info">
+                  <h4>$5914.68</h4>
+                  <p>33 mins ago</p>
+                </div>
+
+                <div class="coin-rank">
+                  <p>1</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+    );
   }
 }
 
