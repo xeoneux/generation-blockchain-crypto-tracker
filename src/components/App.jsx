77f0e37265117ffd8d4dd68d34ed7c8a4af49602
Aayush Kapoor <xeoneux@icloud.com>
2018-06-30 14:54:10 +0530

    Add state to store favourite coins

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,16 @@ import React from 'react';
 import Data from '../data.json';
 
 class App extends React.Component {
+  state = {
+    favourites: ['stellar', 'bitcoin', 'ripple']
+  };
+
+  favouriteCoin = coinId => {
+    this.setState({
+      favourites: this.state.favourites.concat([coinId])
+    });
+  };
+
   render() {
     return (
       <main>
@@ -17,31 +27,34 @@ class App extends React.Component {
 
           <div className="row">
             <ul>
-              {Data.map(coin => (
-                <li key={coin.id} className="coin-item">
-                  <div className="coin-image">
-                    <img
-                      alt={coin.name}
-                      src={require(`cryptocurrency-icons/dist/svg/color/${
-                        coin.symbol
-                      }.svg`)}
-                    />
-                  </div>
+              {this.state.favourites.map(favourite => {
+                const coin = Data.find(coin => coin.id === favourite);
+                return (
+                  <li key={coin.id} className="coin-item">
+                    <div className="coin-image">
+                      <img
+                        alt={coin.name}
+                        src={require(`cryptocurrency-icons/dist/svg/color/${
+                          coin.symbol
+                        }.svg`)}
+                      />
+                    </div>
 
-                  <div className="coin-name">
-                    <p>{coin.name}</p>
-                  </div>
+                    <div className="coin-name">
+                      <p>{coin.name}</p>
+                    </div>
 
-                  <div className="coin-info">
-                    <h4>${coin.price_usd}</h4>
-                    <p>{coin.price_btc}</p>
-                  </div>
+                    <div className="coin-info">
+                      <h4>${coin.price_usd}</h4>
+                      <p>{coin.price_btc}</p>
+                    </div>
 
-                  <div className="coin-rank">
-                    <p>{coin.rank}</p>
-                  </div>
-                </li>
-              ))}
+                    <div className="coin-rank">
+                      <p>{coin.rank}</p>
+                    </div>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </section>
@@ -49,8 +62,14 @@ class App extends React.Component {
         <section>
           <div className="container">
             <div className="row">
-              {Data.map(coin => (
-                <div key={coin.id} className="coin-select col-md-1">
+              {Data.filter(
+                coin => !this.state.favourites.includes(coin.id)
+              ).map(coin => (
+                <div
+                  key={coin.id}
+                  className="coin-select col-md-1"
+                  onClick={() => this.favouriteCoin(coin.id)}
+                >
                   <img
                     alt={coin.name}
                     src={require(`cryptocurrency-icons/dist/svg/color/${
